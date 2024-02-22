98fafb879b3b3251af0f5701629088a9b79ddac0
TroyK12 <101234343+TroyK12@users.noreply.github.com>
2024-02-22 14:27:56 -0600

    update

--- src/app/Navbar/Navbar.tsx
+++ src/app/Navbar/Navbar.tsx
@@ -78,34 +78,3 @@ export default async function Navbar() {
     )
 }
 
-/*         <div className="navbar h-28 border-b-1 m-auto max-w-7xl">
-            <HamburgerMenu />
-            <div className="flex-1">
-                <Link href="/" className="btn btn-ghost normal-case text-xl">
-                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512">
-                        <path d="M0 488V171.3c0-26.2 15.9-49.7 40.2-59.4L308.1 4.8c7.6-3.1 16.1-3.1 23.8 0L599.8 111.9c24.3 9.7 40.2 33.3 40.2 59.4V488c0 13.3-10.7 24-24 24H568c-13.3 0-24-10.7-24-24V224c0-17.7-14.3-32-32-32H128c-17.7 0-32 14.3-32 32V488c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24zm488 24l-336 0c-13.3 0-24-10.7-24-24V432H512l0 56c0 13.3-10.7 24-24 24zM128 400V336H512v64H128zm0-96V224H512l0 80H128z" />
-                    </svg>
-                    GarageShop
-                </Link>
-            </div>
-            <ul className="flex w-full ml-10 gap-3 text-sm">
-                <li className="hover:underline"><Link href={`/productsCategory?data=${encodeURIComponent("hoodie")}`}>Hoodies</Link></li>
-                <li className="hover:underline"><Link href={`/productsCategory?data=${encodeURIComponent("longsleeve")}`}>Long Sleeves</Link></li>
-                <li className="hover:underline"><Link href={`/productsCategory?data=${encodeURIComponent("tshirt")}`}>T-Shirts</Link></li>
-            </ul>
-            <div className="flex-none gap-2">
-                <form action={searchProducts}>
-                    <div className="form-control">
-                        <input
-                            name="searchQuery"
-                            placeholder="Search"
-                            className="input-bordered input w-full min-w-[100px]"
-                        />
-                    </div>
-                </form>
-            </div> 
-            <div className="flex-none">
-                <ShoppingCartBtn cart={cart} />
-                <UserMenuBtn session={session} />
-            </div>
-        </div> */
