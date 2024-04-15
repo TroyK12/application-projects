285cb86fecb867e2baafbebd02d2ea39f6870ff8
TroyK12 <101234343+TroyK12@users.noreply.github.com>
2024-04-15 11:40:20 -0500

    tweek

--- src/app/Navbar/Navbar.tsx
+++ src/app/Navbar/Navbar.tsx
@@ -59,10 +59,12 @@ export default async function Navbar() {
             <div className="navbar-end w-1/3 flex">
                 <div className="dropdown dropdown-end">
                     <button className="sm:btn sm:btn-ghost sm:btn-circle">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                        </svg>
                     </button>
                     <form action={searchProducts}>
-                        <div tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 form-control">
+                        <div tabIndex={0} className="dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 form-control">
                             <input
                                 name="searchQuery"
                                 placeholder="Search"
